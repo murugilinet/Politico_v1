06f8fc2e708e4a0896f7abf73e3b1ca1bb00bc94
murugilinet <murugilinet59@gmail.com>
2019-02-07 14:44:10 +0300

    starts [#163781297] adds an endpoint of admin to create party

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint 
 from flask_restful import Api,Resource
 from app.api.v1.view.office_views import Offices,Office
+from app.api.v1.view.party_views  import Parties
 
 version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
@@ -8,4 +9,5 @@ api = Api(version_one)
 
 api.add_resource (Offices, '/offices')
 api.add_resource (Office, '/office/<int:office_id>')
+api.add_resource (Parties, '/parties')
 
